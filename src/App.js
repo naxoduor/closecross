4a4122b2befe9fdec7feb4cea55ed4959043641e
Nax Oduor <nax@smartmile.eu>
2021-01-02 23:13:50 +0300

    add containers

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Grid } from '@material-ui/core';
 import './App.css';
+import ButtonsContainer from './components/containers/ButtonsContainer'
 import Button from './components/ButtonComponent'
 import Icon from './components/Icons'
+import SectionsContainer from './components/containers/SectionsContainer'
 import Section from './components/section'
 import SideNav from './components/sidenav'
 import TopMainSection from './components/topmainsection'
@@ -15,7 +17,7 @@ function App() {
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    margin:"auto"
+    margin: "auto"
   }
   const style = {
     backgroundColor: "#1E2122",
@@ -30,22 +32,9 @@ function App() {
     backgroundColor: "#494947",
     height: "64px",
     width: "1280px",
-    position:"relative",
-    borderBottom:"2px solid green",
-    margin:"auto"
-  }
-
-  const innerstyle = {
-    backgroundColor: "#27A2D",
-    height: "88px",
-    width: "930px",
-    alignItems: "center",
-    color: "white",
-    margin: "auto",
-    marginTop: "10px",
-    marginBottom: "10px",
-    border:"1px solid grey",
-    borderRadius:"25px"
+    position: "relative",
+    borderBottom: "2px solid green",
+    margin: "auto"
   }
   const sidepanel = {
     backgroundColor: "#18181C",
@@ -54,44 +43,39 @@ function App() {
     marginLeft: "0px",
   }
 
-  const buttonStyle={
-    position:"absolute",
-    top:"25%",
+  const buttonStyle = {
+    position: "absolute",
+    top: "25%",
   }
 
 
   return (
     <div className="App" style={{ alignItems: "center" }}>
-        <div style={topstyle}>
-          <Grid container justify="flex-start" style={buttonStyle}>
-            <Button bgcolor="white" color="#494947" variant="caption" text="Training Mode"/>
-            <Button bgcolor="#494947" color="white" variant="caption" text="Live Mode"/>
-          </Grid>
-          <Grid container justify="center" style={buttonStyle}>
-            <Button bgcolor="white" color="#494947" variant="button" text="Show All"/>
-            <Button bgcolor="#494947" color="white" variant="button" text="Crypto"/>
-            <Button bgcolor="white" color="#494947" variant="button" text="Commodities"/>
-            <Button bgcolor="#494947" color="white" variant="button" text="Stock"/>
-            <Button bgcolor="white" color="#494947" variant="button" text="Index"/>
-            <Button bgcolor="#494947" color="white" variant="button" text="Currency"/>
-          </Grid>
-          <Grid container justify="flex-end" style={buttonStyle}>
+      <div style={topstyle}>
+
+        <Grid container justify="flex-start" style={buttonStyle}>
+          <Button bgcolor="white" color="#494947" variant="caption" text="Training Mode" />
+          <Button bgcolor="#494947" color="white" variant="caption" text="Live Mode" />
+        </Grid>
+
+        <Grid container justify="center" style={buttonStyle}>
+          <ButtonsContainer />
+        </Grid>
+
+        <Grid container justify="flex-end" style={buttonStyle}>
           <Icon />
-          </Grid>
+        </Grid>
+      </div>
+      
+      <div style={outerpanel}>
+        <div style={sidepanel}>
+          <SideNav />
         </div>
-        <div style={outerpanel}>
-          <div style={sidepanel}>
-            <SideNav/>
-          </div>
-          <div style={style}>
-            <div><TopMainSection/></div>
-            <div style={innerstyle}><Section amount="$ 34,346.00"/></div>
-            <div style={innerstyle}><Section amount="$ 329,146.00"/></div>
-            <div style={innerstyle}><Section amount="$ 129,146.00"/></div>
-            <div style={innerstyle}><Section amount="$ 46.00"/></div>
-            <div style={innerstyle}><Section amount="$ 00.00"/></div>
-          </div>
+        <div style={style}>
+          <div><TopMainSection /></div>
+          <SectionsContainer />
         </div>
+      </div>
     </div>
   );
 }
